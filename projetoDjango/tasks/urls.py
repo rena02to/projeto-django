6f89b8f3e677ffce4b068d62640587c7903b22be
rena02to <rena02to@gmail.com>
2024-06-26 22:40:02 -0300

    adicionando os arquivos

--- projetoDjango/tasks/urls.py
+++ projetoDjango/tasks/urls.py
@@ -1,8 +1,13 @@
 from django.urls import path
+
 from . import views
 
 urlpatterns = [
-    path('helloworld/', views.helloWorld),
     path('', views.taskList, name='task-list'),
-    path('yourname/<str:name>', views.yourName, name='your-name')
+    path('task/<int:id>/', views.taskView, name="task-view"),
+    path('newtask/', views.newTask, name="new-task"),
+    path('edit/<int:id>', views.editTask, name="edit-task"),
+
+    path('helloworld/', views.helloWorld),
+    path('yourname/<str:name>', views.yourName, name='your-name'),
 ]
--- projetoDjango/tasks/views.py
+++ projetoDjango/tasks/views.py
@@ -1,11 +1,38 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
 from django.http import HttpResponse
-
-def helloWorld(request):
-    return HttpResponse('Hello world!')
+from .forms import TaskForm
+from .models import Task
 
 def taskList(request):
-    return render(request, 'tasks/list.html')
+    task = Task.objects.all().order_by('-created_at')
+    return render(request, 'tasks/list.html', {'tasks': task})
+
+def taskView(request, id):
+    task = get_object_or_404(Task, pk=id)
+    return render(request, 'tasks/task.html', {'task': task})
+
+def newTask(request):
+    if request.method == 'POST':
+        form = TaskForm(request.POST)
+        if form.is_valid():
+            task = form.save(commit=False)
+            task.done = 'doing'
+            task.save()
+            return redirect('/')
+    else:
+        form = TaskForm()
+        return render(request, 'tasks/addtask.html', {'form': form})
+    
+def editTask(request, id):
+    task = get_object_or_404(Task, pk=id)
+    form = TaskForm(instance=task)
+    if(request.method == 'POST'):
+        return False
+    else:
+        return render(request, 'task/edittask.html', {'form': form, 'task': task})
+
+def helloWorld(request):
+    return HttpResponse('Hello World!')
 
 def yourName(request, name):
-    return render(request, 'tasks/yourname.html', {'name': name})
+    return render(request, 'tasks/yourname.html', {'name':name})
